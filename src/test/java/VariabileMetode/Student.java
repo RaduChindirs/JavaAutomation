c93090a5221e8c6ce05cd7f71b0eaec8543f5d21
Radu Chindris <radu.chindris@endava.com>
2024-12-04 14:16:35 +0200

    Radu 1

---
+++ src/test/java/VariabileMetode/Student.java
@@ -0,0 +1,13 @@
+package VariabileMetode;
+
+public class Student {
+
+        public String Nume;
+        public String Prenume;
+        public Integer Varsta;
+        public String Adresa;
+        public Double Inaltime;
+        public Float Greutate;
+        public Character Sex;
+        public Boolean AreDiploma;
+}
